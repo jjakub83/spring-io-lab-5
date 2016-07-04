4c883c76e6d5babf5c036bdff389e547c51b5adf
Darek Kaczyński <kaczynskid@gmail.com>
2016-07-04 12:42:04 +0200

    reservation-service from lab 3

--- reservation-service/src/main/java/com/example/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/ReservationServiceApplication.java
@@ -1,20 +1,34 @@
 package com.example;
 
+import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import java.net.URI;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,11 +45,37 @@ public class ReservationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner init(ReservationRepository repository) {
+		return args -> Arrays.asList("Krzysiek,Marcin,Kamil,Emil,Tomek,Krzysiek,Michał,Patryk,Kuba".split(","))
+				.forEach(name -> repository.save(new Reservation(name)));
+	}
+
+}
+
+@Component
+class ReservationResourceProcessor implements ResourceProcessor<Resource<Reservation>> {
+
+	@Override
+	public Resource<Reservation> process(Resource<Reservation> resource) {
+		Reservation reservation = resource.getContent();
+		String url = format("https://www.google.pl/search?tbm=isch&q=%s", reservation.getName());
+		resource.add(new Link(url, "photo"));
+		return resource;
+	}
+}
+
+@RepositoryRestResource
+interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+	@RestResource(path = "by-name", rel = "find-by-name")
+	List<Reservation> findByName(@Param("rn") String name);
 }
 
 @Slf4j
 @RestController
-@RequestMapping("/reservations")
+@RequestMapping("/custom/reservations")
 class ReservationController {
 
 	List<Reservation> storage =
@@ -49,11 +89,12 @@ class ReservationController {
 
 	@RequestMapping(path = "/{name}", method = GET)
 	public ResponseEntity<?> getReservation(@PathVariable String name) {
-		Optional<ResponseEntity<?>> optional = storage.stream()
+		Optional<ResponseEntity<Resource>> optional = storage.stream()
 				.filter(r -> r.name.equals(name))
-				.findFirst()
 				.map(r -> new Resource(r, new Link(selfURI(r).toString(), "self")))
-				.map(r -> ResponseEntity.ok(r));
+				.map(r -> ResponseEntity.ok(r))
+				.findFirst();
+		//.orElse(ResponseEntity.notFound().build());
 		return optional.isPresent() ? optional.get() : ResponseEntity.notFound().build();
 	}
 
@@ -64,19 +105,27 @@ class ReservationController {
 		return ResponseEntity.created(selfURI(reservation)).build();
 	}
 
-	private URI selfURI(Reservation reservation) {
+	URI selfURI(Reservation reservation) {
 		return ControllerLinkBuilder.linkTo(
 				ControllerLinkBuilder.methodOn(ReservationController.class).getReservation(reservation.name))
 			.toUri();
 	}
-
 
 }
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
 class Reservation {
 
+	@Id
+	@GeneratedValue
+	Long id;
+
 	String name;
+
+	Reservation(String name) {
+		this.name = name;
+	}
 }
